1dd9f875ddabee393b91405b7ea5535d884e03a5
hyamakaw <hirotoyamakawa@yamakawatakutos-MacBook-Air.local>
2024-02-23 22:22:55 +0900

    add: zip_code

--- etl/scraping.py
+++ etl/scraping.py
@@ -7,7 +7,7 @@ page = requests.get("https://www.rairaitei.co.jp/store/list.php")
 soup = BeautifulSoup(page.content, "html.parser")
 stores = soup.find_all("div", class_="store_list_box")
 
-names, addresses, prefectures = [], [], []
+names, addresses, prefectures, zip_codes = [], [], [], []
 for store in stores:
     name = store.h4.text
 
@@ -16,9 +16,13 @@ for store in stores:
     prefecture_element = re.match('東京都|北海道|(?:京都|大阪)府|.{2,3}県' , address)
     prefecture = prefecture_element.group() if prefecture_element else None
 
+    zip_code_element = requests.get(f'https://api.excelapi.org/post/zipcode?address={address}')
+    zip_code = zip_code_element.text if zip_code_element else None
+
     names.append(name)
     addresses.append(address)
     prefectures.append(prefecture)
+    zip_codes.append(zip_code)
 
-df = pd.DataFrame({'name': names, 'address': addresses, 'prefectures': prefectures})
+df = pd.DataFrame({'name': names, 'address': addresses, 'prefectures': prefectures, 'zip_code': zip_codes})
 df.to_csv('rairaitei.csv', index=False, encoding='utf-8')
