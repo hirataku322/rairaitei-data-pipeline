db16cbb3b823b5f7a2643524c13fc32e54283801
hyamakaw <hirotoyamakawa@yamakawatakutos-MacBook-Air.local>
2024-02-23 18:57:47 +0900

    add: prefecture

--- etl/scraping.py
+++ etl/scraping.py
@@ -1,14 +1,24 @@
+import re
 import requests
+import pandas as pd
 from bs4 import BeautifulSoup
 
 page = requests.get("https://www.rairaitei.co.jp/store/list.php")
 soup = BeautifulSoup(page.content, "html.parser")
 stores = soup.find_all("div", class_="store_list_box")
 
-names = []
-addresses = []
+names, addresses, prefectures = [], [], []
 for store in stores:
-    names.append(store.h4.text)
-    addresses.append(store.find_all("li", class_="store_add")[0].text)
+    name = store.h4.text
 
-print(names)
+    address = store.find("li", class_="store_add").text.replace('住所', '')
+
+    prefecture_element = re.match('東京都|北海道|(?:京都|大阪)府|.{2,3}県' , address)
+    prefecture = prefecture_element.group() if prefecture_element else None
+
+    names.append(name)
+    addresses.append(address)
+    prefectures.append(prefecture)
+
+df = pd.DataFrame({'name': names, 'address': addresses, 'prefectures': prefectures})
+print(df)
